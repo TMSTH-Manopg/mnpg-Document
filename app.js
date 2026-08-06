b819b319b5db2289fcc3d071cf1402bf3c73625a
TMSTH-Manopg <manopg@tokiomarinesafety.co.th>
2026-08-06 15:35:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -182,20 +182,3 @@ function formatTitle(folderName) {
     .replace(/_/g, " ")
     .replace(/-/g, " ");
 }
-
-let mybutton = document.getElementById("btn-back-to-top");
-
-window.onscroll = function () {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-};
-
-mybutton.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-});
